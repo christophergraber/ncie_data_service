c21dcbac27b0c3497306c019f461d20ddd434114
Chris Graber <christophergraber@gmail.com>
2021-11-05 13:34:33 -0700

    adding info

--- controller.py
+++ controller.py
@@ -1,8 +1,8 @@
 from c_ncie_data_service_api import c_ncie_data_service_api
 
-api_result = c_ncie_data_service_api('daily-summaries', 'TMIN,TMAX,TOBS,DAPR,MDPR,PRCP,SNOW,SNWD,AWND,ACMH,ACSH,PSUN', 'USW00023188', '2021-01-01', '2021-10-15', '90,-180,-90,180', 'json')
+api_result = c_ncie_data_service_api('daily-summaries', 'TMIN,TMAX,TOBS,DAPR,MDPR,PRCP,SNOW,SNWD,AWND,ACMH,ACSH,PSUN', 'USW00012921', '2021-01-01', '2021-11-05', '90,-180,-90,180', 'json')
 print(api_result.get_data())
-api_result.write_data_file('weather_data_san_diego3.txt')
+api_result.write_data_file('weather_data_san_antonio.json')
 
 # National Data Buoy Center: https://www.ndbc.noaa.gov/ (Examples: AUCE, WHRI2)
 # Rochester International Airport: USW00014925; Chicago O'Hare International Airport: USW00094846; San Jose International Airport: USW00023293
